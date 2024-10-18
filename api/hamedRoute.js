3d724ab92c0edc31f868d03401d3d557d1d3811d
Ahmed Khaled <ahmedkhaled.fouda@dxc.com>
2024-10-18 17:39:41 +0300

    s

--- api/hamedRoute.js
+++ api/hamedRoute.js
@@ -2,49 +2,26 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, node/no-extraneous-require
 const express = require("express");
 const {
-  getShipments,
-  createShipment,
-  getShipment,
-  updateShipment,
-  deleteShipment,
-  resizeImage,
-  uploadCategoryImage,
-} = require("../services/shipment.service");
+create,
+get,
+deleteOne,
+getAll,
+update
+} = require("../services/hamed.service");
 
 const { allowedTo, protect } = require("../services/auth.service");
 
 const router = express.Router();
 
-// const {
-//   getProductValidator,
-//   createProductValidator,
-//   updateProductValidator,
-//   deleteProductValidator,
-// } = require("../utils/productsVaildator");
-/**
- * Express Router for managing category-related routes.
- * @module routes/category
- */
-
-/**
- * Route to get category data.
- * @name GET /category
- * @function
- * @memberof module:routes/category
- * @param {string} path - Express path.
- * @param {callback} middleware - Express middleware.
- * @returns {object} - The response object.
- */
-
 // Define routes for handling category operations
 router
   .route("/")
-  .get( getShipments)
-  .post(createShipment);
+  .get( getAll)
+  .post(create);
 router
   .route("/:id")
-  .get( getShipment)
-  .put(updateShipment)
-  .delete( deleteShipment);
+  .get( get)
+  .put(update)
+  .delete( deleteOne);
 
 module.exports = router;
